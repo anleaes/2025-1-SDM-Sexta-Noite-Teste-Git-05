522a7cbbc945f93a9d20744db5abe8f333608916
Victorpa12 <victor.paltnetter@hotmail.com>
2025-03-20 17:52:10 -0300

    criação da classe cliente

---
+++ cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, endereco, telefone, email, genero):
+        self._endereco = endereco
+        self._telefone = telefone
+        self._email = email
+        self._genero = genero
